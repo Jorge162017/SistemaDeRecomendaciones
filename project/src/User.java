30ffc7a73cd181dabb003ce9a35cf9e5d60c39d1
Jose Gramajo <gra22907@uvg.edu.gt>
2023-05-31 23:33:25 -0600

    Javadoc User class

--- project/src/User.java
+++ project/src/User.java
@@ -6,10 +6,19 @@
 package projectequipos;
 
 /**
- *
- * @author Jose Gramajo
+ * The User class represents a user in the system.
+ * It contains information such as name, username, password, favorite Liga, and country.
  */
 public class User {
+    /**
+     * Constructs a new User object with the specified information.
+     *
+     * @param name the name of the user
+     * @param username the username of the user
+     * @param password the password of the user
+     * @param favoriteLiga the favorite Liga of the user
+     * @param pais the country of the user
+     */
     private String name;
     private String username;
     private String password;
@@ -24,48 +33,93 @@ public class User {
         this.pais = pais;
     }
 
-
-
-
+    /**
+     * Returns the name of the user.
+     *
+     * @return the name of the user
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * Sets the name of the user.
+     *
+     * @param name the name of the user
+     */
     public void setName(String name) {
         this.name = name;
     }
 
+    /**
+     * Returns the username of the user.
+     *
+     * @return the username of the user
+     */
     public String getUsername() {
         return username;
     }
 
+    /**
+     * Sets the username of the user.
+     *
+     * @param username the username of the user
+     */
     public void setUsername(String username) {
         this.username = username;
     }
 
+    /**
+     * Returns the password of the user.
+     *
+     * @return the password of the user
+     */
     public String getPassword() {
         return password;
     }
 
+    /**
+     * Sets the password of the user.
+     *
+     * @param password the password of the user
+     */
     public void setPassword(String password) {
         this.password = password;
     }
 
+    /**
+     * Returns the favorite Liga of the user.
+     *
+     * @return the favorite Liga of the user
+     */
     public String getFavoriteLiga() {
         return favoriteLiga;
     }
 
+    /**
+     * Sets the favorite Liga of the user.
+     *
+     * @param favoriteLiga the favorite Liga of the user
+     */
     public void setFavoriteLiga(String favoriteLiga) {
         this.favoriteLiga = favoriteLiga;
     }
 
+    /**
+     * Returns the country of the user.
+     *
+     * @return the country of the user
+     */
     public String getPais() {
         return pais;
     }
 
+    /**
+     * Sets the country of the user.
+     *
+     * @param pais the country of the user
+     */
     public void setPais(String pais) {
         this.pais = pais;
     }
-
-
 }

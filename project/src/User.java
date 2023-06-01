fbf91c41337a5170096ebd304ab08c13a13ab87c
Jose Gramajo <gra22907@uvg.edu.gt>
2023-05-31 23:15:29 -0600

    User class

--- project/src/User.java
+++ project/src/User.java
@@ -1,2 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projectequipos;
+
+/**
+ *
+ * @author Jorge
+ */
 public class User {
+    private String name;
+    private String username;
+    private String password;
+    private String favoriteLiga;
+    private String pais;
+
+    public User(String name, String username, String password, String favoriteLiga, String pais) {
+        this.name = name;
+        this.username = username;
+        this.password = password;
+        this.favoriteLiga = favoriteLiga;
+        this.pais = pais;
+    }
 }
